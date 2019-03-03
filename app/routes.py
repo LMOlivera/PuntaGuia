cf6e541a01bf9dd41c4e156a344c4990afa64bfb
Lucas Olivera <42621132+LeWanderer@users.noreply.github.com>
2019-03-02 21:22:43 -0300

    Implementación de requerimiento 4
    Ya se puede eliminar al usuario. Elimina al usuario de la tabla "usuario" y de "empresa" o "turista" según su tipo. Esta acción no se puede revertir.

--- app/routes.py
+++ app/routes.py
@@ -15,80 +15,85 @@ connection = pymysql.connect(host='localhost',
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def bienvenido():
-    form = LoginForm() # Formulario de forms.py, se importa
-    if form.validate_on_submit(): #Al submitear data se corre esto
-        with connection.cursor() as cursor:
-            email=form.email.data
-            password=form.password.data
-            query = "SELECT id_usuario, email, nombre, tipo FROM usuario WHERE email=%s AND contrasena=%s"
-            cursor.execute(query,(email,password))
-            userdata = cursor.fetchone()
-            results = cursor.rowcount
-            if results==0:
-                flash('Usuario y/o contraseña inválidos')                
-                session['invalid_user']="true"               
-                return redirect('/')
-            else:
-                session.pop('invalid_user', None)
-                session['logueado']=True
-                session['id_usuario'] = userdata['id_usuario']
-                session['email'] = userdata['email']
-                session['nombre'] = userdata['nombre']
-                session['tipo'] = userdata['tipo']
-                return redirect('/principal')
+    if session.get('logueado'):
+        return redirect('/principal')
+    else:        
+        form = LoginForm() # Formulario de forms.py, se importa
+        if form.validate_on_submit(): #Al submitear data se corre esto
+            with connection.cursor() as cursor:
+                email=form.email.data
+                password=form.password.data
+                query = "SELECT id_usuario, email, nombre, tipo FROM usuario WHERE email=%s AND contrasena=%s"
+                cursor.execute(query,(email,password))
+                userdata = cursor.fetchone()
+                results = cursor.rowcount
+                if results==0:
+                    flash('Usuario y/o contraseña inválidos')                
+                    session['invalid_user']="true"               
+                    return redirect('/')
+                else:
+                    session.pop('invalid_user', None)
+                    session['logueado']=True
+                    session['id_usuario'] = userdata['id_usuario']
+                    session['email'] = userdata['email']
+                    session['nombre'] = userdata['nombre']
+                    session['tipo'] = userdata['tipo']
+                    return redirect('/principal')
     return render_template('bienvenido.html', form=form)
 
 @app.route('/index/registro', methods=['GET', 'POST'])
 def registro():
-    nuevo = request.args.to_dict()
-    form = RegisterForm()
-    if form.validate_on_submit():
-        try:
-            with connection.cursor() as cursor:
-                email = form.email.data
-                nombre = form.nombre.data
-                password = form.password.data
-                tipo = nuevo['tipo']
-                query = """
-                INSERT INTO usuario
-                (email,nombre,contrasena,tipo)
-                VALUES
-                (%s,%s,%s,%s)
-                """
-                cursor.execute(query,(email,nombre,password,tipo))
-                query = """
-                SELECT id_usuario FROM usuario
-                WHERE email=%s
-                """
-                cursor.execute(query,(email))
-                userdata = cursor.fetchone()
-                id_usuario=userdata['id_usuario']
+    if session.get('logueado'):
+        return redirect('/principal')   
+    else:        
+        nuevo = request.args.to_dict()
+        form = RegisterForm()
+        if form.validate_on_submit():
+            try:
+                with connection.cursor() as cursor:
+                    email = form.email.data
+                    nombre = form.nombre.data
+                    password = form.password.data
+                    tipo = nuevo['tipo']
+                    query = """
+                    INSERT INTO usuario
+                    (email,nombre,contrasena,tipo)
+                    VALUES
+                    (%s,%s,%s,%s)
+                    """
+                    cursor.execute(query,(email,nombre,password,tipo))
+                    query = """
+                    SELECT id_usuario FROM usuario
+                    WHERE email=%s
+                    """
+                    cursor.execute(query,(email))
+                    userdata = cursor.fetchone()
+                    id_usuario=userdata['id_usuario']
 
-                if nuevo['tipo']=='turista':
-                    edad = form.edad.data
-                    pais = form.pais.data
-                    query = """
-                    INSERT INTO turista
-                    (id,edad,pais_origen)
-                    VALUES
-                    (%s,%s,%s)
-                    """
-                    cursor.execute(query,(id_usuario,edad,pais))
-                else:
-                    nombreEmpresa = form.nombreEmpresa.data
-                    query = """
-                    INSERT INTO empresa
-                    (id,nombre)
-                    VALUES
-                    (%s,%s)
-                    """
-                    cursor.execute(query,(id_usuario,nombreEmpresa))
-            connection.commit()
-            session.clear()
-            return redirect('/index') 
-        except NameError:    
-                print('Algo malo acaba de ocurrir: ' + NameError)
-        
+                    if nuevo['tipo']=='turista':
+                        edad = form.edad.data
+                        pais = form.pais.data
+                        query = """
+                        INSERT INTO turista
+                        (id,edad,pais_origen)
+                        VALUES
+                        (%s,%s,%s)
+                        """
+                        cursor.execute(query,(id_usuario,edad,pais))
+                    else:
+                        nombreEmpresa = form.nombreEmpresa.data
+                        query = """
+                        INSERT INTO empresa
+                        (id,nombre)
+                        VALUES
+                        (%s,%s)
+                        """
+                        cursor.execute(query,(id_usuario,nombreEmpresa))
+                connection.commit()
+                session.clear()
+                return redirect('/index') 
+            except NameError:    
+                    print('Algo malo acaba de ocurrir: ' + NameError)         
     return render_template('registro.html', form=form,tipo=nuevo['tipo'])
 
 @app.route('/principal')
@@ -100,7 +105,39 @@ def index():
         return redirect('/')
     return render_template('principal.html')
 
+@app.route('/principal/usuario')
+def usuario():
+    if session.get('logueado'):
+        print('ok')
+    else:
+        session.clear()
+        return redirect('/')
+    return render_template('usuario.html')
+
+
 @app.route('/logout')
 def logout():
-    session.clear()
+    try:
+        idu = request.args.to_dict()
+        iduINT = int(idu['id_usuario'])
+        ses = int(session['id_usuario'])
+        with connection.cursor() as cursor:
+            query = """
+                    DELETE FROM usuario WHERE id_usuario=%s
+                    """ 
+            cursor.execute(query,(ses))
+            if session['tipo']=='turista':
+                query = """
+                        DELETE FROM turista WHERE id=%s
+                        """ 
+                cursor.execute(query,(ses))
+            else:
+                query = """
+                    DELETE FROM empresa WHERE id=%s
+                    """ 
+                cursor.execute(query,(ses))
+        connection.commit()
+    except:
+        print("Logout")           
+    session.clear()        
     return redirect('/')
