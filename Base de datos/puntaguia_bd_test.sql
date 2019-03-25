d3201ffeaa350fec3a78922f44349a94b0e13422
Lucas Olivera <42621132+LMOlivera@users.noreply.github.com>
2019-03-25 15:27:54 -0300

    Cambio de nombre
    Para evitar conflictos futuros.

--- Base de datos/puntaguia_bd_test.sql
+++ Base de datos/puntaguia_bd_test.sql
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de datos: `uruguia_bd_test`
+-- Base de datos: `puntaguia_bd_test`
 --
 
 -- --------------------------------------------------------
